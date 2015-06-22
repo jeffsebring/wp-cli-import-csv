7aac5ac3a5eda4f11f0d6b69173241d79bba8ec6
Scott Kingsley Clark <lol@scottkclark.com>
2015-06-22 15:23:17 -0500

    Fix var name

--- import-csv.php
+++ import-csv.php
@@ -328,7 +328,7 @@ class ImportCSV_Command extends WP_CLI_Command {
 			}
 			
 			// Rebuild $header[ 2 ] so it supports keys and taxonomies with dashes
-			$header[ 2 ] = implode( '-', array_slice( $headers, 2 ) );
+			$header[ 2 ] = implode( '-', array_slice( $header, 2 ) );
 
 			if ( ! isset( $header[ 0 ] ) || $header[ 0 ] == 'taxonomy' && ! taxonomy_exists( $header[ 2 ] ) ) {
 
